9624189b9e764483b2191ea57e9322c6f5e6d80c
Jens Torgeir Solaugsten <torger@solaugsten.no>
2020-04-20 08:58:38 +0200

    Initial Commit

---
+++ PowershellScriptTemplate.ps1
@@ -0,0 +1,46 @@
+#requires -version 2
+<#
+.SYNOPSIS
+    <Overview of script>
+
+.DESCRIPTION
+    <Brief description of script>
+
+.PARAMETER <Parameter_Name>
+    <Brief description of parameter input. Repeat if necessary>
+
+.INPUTS
+    <Inputs if any, otherwise set "None" here>
+
+.OUTPUTS
+    <Outputs if any, otherwise set "None" here>
+
+.NOTES
+    Version:        1.0
+    Author:         <Name>
+    Creation date:  <Date>
+    Repository:     <https://www.github.com/...>
+    History:        <Date>: <Name> - Initial script development
+    
+.EXAMPLE
+    <Example(s) of script usage.>
+#>
+Function FunctionName{  #Functionname Identical with filename
+    Param()
+
+    Begin{
+        # Initialization code goes here
+    }
+
+    Process{
+        Try {
+            # Code goes here :-)
+        }
+        Catch {
+            # Code goes here
+        }
+    }
+    End {
+        # Code Goes here (remove Begin/Process/End if not necessary)
+    }
+}
